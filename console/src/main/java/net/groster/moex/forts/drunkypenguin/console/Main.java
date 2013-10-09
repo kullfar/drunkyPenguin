d13cfecc918b2a4453da8fc4738aefe80741677e
Stas Gromov <s.gromov@hh.ru>
2013-10-09 18:57:31 +0400

    add module 'console' with simple Console Application

---
+++ console/src/main/java/net/groster/moex/forts/drunkypenguin/console/Main.java
@@ -0,0 +1,8 @@
+package net.groster.moex.forts.drunkypenguin.console;
+
+public abstract class Main {
+
+    public static void main(final String[] args) {
+        System.out.println("Yahoo!");
+    }
+}
