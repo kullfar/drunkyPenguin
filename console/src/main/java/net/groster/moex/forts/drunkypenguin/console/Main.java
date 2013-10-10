9346bea17a3131d59a5dd0a73f0306c699a3d928
Stas Gromov <s.gromov@hh.ru>
2013-10-10 16:14:43 +0400

    add spring context and add fast config url to spring-core.xml + put Updater in spring context

--- console/src/main/java/net/groster/moex/forts/drunkypenguin/console/Main.java
+++ console/src/main/java/net/groster/moex/forts/drunkypenguin/console/Main.java
@@ -1,10 +1,14 @@
 package net.groster.moex.forts.drunkypenguin.console;
 
 import net.groster.moex.forts.drunkypenguin.core.config.Updater;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public abstract class Main {
 
     public static void main(final String[] args) {
-        new Thread(new Updater()).start();
+        final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("spring-core.xml");
+        context.registerShutdownHook();
+        context.getBean(Updater.class).start();
     }
 }
--- core/src/main/java/net/groster/moex/forts/drunkypenguin/core/config/Updater.java
+++ core/src/main/java/net/groster/moex/forts/drunkypenguin/core/config/Updater.java
@@ -1,17 +1,26 @@
 package net.groster.moex.forts.drunkypenguin.core.config;
 
+import java.io.File;
+import javax.annotation.Resource;
+import javax.inject.Named;
+import javax.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Updater implements Runnable {
+@Named
+@Singleton
+public class Updater extends Thread {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Updater.class);
     private boolean continueWorking = true;
+    @Resource
+    private String fastConfPath;
 
     @Override
     public void run() {
         while (continueWorking) {
-            LOGGER.info("I was here");
+            LOGGER.info("I was here and {'fastConfPath'='" + fastConfPath + "'}.");
+            LOGGER.info("isFile=" + new File("configuration.xml").isFile());
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException iE) {
