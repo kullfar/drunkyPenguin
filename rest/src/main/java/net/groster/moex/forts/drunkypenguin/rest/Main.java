d48bbbbf0c048f7d1e2ef2061964797b2b6c5939
Stas Gromov <kullfar@gmail.com>
2013-12-04 14:07:00 +0400

    make all jersey resources singletons and spring DI injected

--- rest/src/main/java/net/groster/moex/forts/drunkypenguin/rest/Main.java
+++ rest/src/main/java/net/groster/moex/forts/drunkypenguin/rest/Main.java
@@ -2,32 +2,54 @@ package net.groster.moex.forts.drunkypenguin.rest;
 
 import java.io.IOException;
 import java.net.URI;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
 import net.groster.moex.forts.drunkypenguin.core.fast.FASTService;
 import net.groster.moex.forts.drunkypenguin.core.fast.config.FASTConfigsUpdatesChecker;
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpContainer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
-public abstract class Main {
+@Named
+@Singleton
+public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    @Resource
+    private String restServiceBaseUri;
+    private static String staticRestServiceBaseUri;
+    @Inject
+    private ConfigurableApplicationContext springContext;
+    private static ConfigurableApplicationContext staticSpringContext;
+
+    @PostConstruct
+    public void postConstruct() {
+        staticRestServiceBaseUri = restServiceBaseUri;
+        staticSpringContext = springContext;
+    }
 
     public static void main(final String[] args) throws IOException {
         SLF4JBridgeHandler.removeHandlersForRootLogger();
         SLF4JBridgeHandler.install();
 
         try {
-            final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("spring-rest.xml");
-            context.registerShutdownHook();
-            final String restServiceBaseUri = (String) context.getBean("restServiceBaseUri");
+            final ResourceConfig resourceConfig = new ResourceConfig().register(StatusResource.class).property(
+                    "contextConfigLocation", "spring-rest.xml");
+            final GrizzlyHttpContainer grizzlyHttpContainer = ContainerFactory.createContainer(
+                    GrizzlyHttpContainer.class, resourceConfig);
+            staticSpringContext.registerShutdownHook();
 
-            final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(restServiceBaseUri),
-                    new ResourceConfig().register(StatusResource.class));
+            final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(staticRestServiceBaseUri),
+                    grizzlyHttpContainer, false, null, true);
 
             Runtime.getRuntime().addShutdownHook(new Thread() {
                 @Override
@@ -37,10 +59,12 @@ public abstract class Main {
                     LOGGER.info("Grizzly server with Jersey app was stopped");
                 }
             });
-            LOGGER.info("Jersey app started with WADL available at " + restServiceBaseUri + "application.wadl");
+            LOGGER.info("Jersey app started with WADL available at " + staticRestServiceBaseUri + "application.wadl");
 
-            context.getBean(FASTConfigsUpdatesChecker.class).start();
-            context.getBean(FASTService.class).start();
+            LOGGER.info("Starting FAST threads");
+            staticSpringContext.getBean(FASTConfigsUpdatesChecker.class).start();
+            staticSpringContext.getBean(FASTService.class).start();
+            LOGGER.info("Started FAST threads");
 
         } catch (Throwable t) {
             LOGGER.error("All is bad", t);
--- rest/src/main/java/net/groster/moex/forts/drunkypenguin/rest/StatusResource.java
+++ rest/src/main/java/net/groster/moex/forts/drunkypenguin/rest/StatusResource.java
@@ -1,11 +1,13 @@
 package net.groster.moex.forts.drunkypenguin.rest;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("status")
+@Singleton
 public class StatusResource {
 
     @GET
