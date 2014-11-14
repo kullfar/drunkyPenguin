562e7825ee3f87b8614c9262d5b8d9cb39528e7e
Stas Gromov <kullfar@gmail.com>
2014-11-14 20:45:39 +0300

    remove MaxPermSize option because of "ignoring option MaxPermSize=26m; support was removed in 8.0"

--- console/src/scripts/start.sh
+++ console/src/scripts/start.sh
@@ -1,1 +1,1 @@
-java -XX:MaxPermSize=1m -Xmx10m -jar console-4.0.0-SNAPSHOT.jar
+java -Xmx10m -jar console-4.0.0-SNAPSHOT.jar
--- rest/src/scripts/start.sh
+++ rest/src/scripts/start.sh
@@ -1,1 +1,1 @@
-java -XX:MaxPermSize=26m -Xmx20m -jar rest-4.0.0-SNAPSHOT.jar
+java -Xmx20m -jar rest-4.0.0-SNAPSHOT.jar
